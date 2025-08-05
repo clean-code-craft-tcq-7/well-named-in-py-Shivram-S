283c73b58334cc1599e523235e9639acc841ecf1
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-08-05 12:32:17 +0530

    Updated imports

--- test_color_code.py
+++ test_color_code.py
@@ -1,4 +1,4 @@
-import color_code_num
+from color_code_num import get_color_from_pair_number,get_pair_number_from_color
 
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
